7adc8421455d996d34b030da57dca4be4b373b83
ShreyaMohan07 <90245952+ShreyaMohan07@users.noreply.github.com>
2025-03-04 08:56:07 +0530

    Update SampleTest.java

--- src/test/java/com/qspiders/book/SampleTest.java
+++ src/test/java/com/qspiders/book/SampleTest.java
@@ -3,7 +3,7 @@ package com.qspiders.book;
 public class SampleTest {
 private void createOrgTest() {
 	// TODO Auto-generated method stub
-	System.out.println("execute create org");
+	System.out.println("execute create o");
 
 }
 }
